cdc0b5af9aeff2f1ebaa24257137eef757acfc22
iTrooz <itrooz@protonmail.com>
2022-08-17 02:36:54 +0200

    initial commit

---
+++ build.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+NMAP_SVN="https://svn.nmap.org/nmap-releases/"
+DIR=$(dirname $(realpath $0))
+
+echo "--- Preparing linuxdeploy..."
+curl https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage -o linuxdeploy -L
+chmod +x linuxdeploy
+
+echo "--- Cloning latest version of nmap..."
+NMAP_VER_FOLDER=`svn ls $NMAP_SVN | tail -n 1`
+svn checkout $NMAP_SVN/$NMAP_VER_FOLDER $DIR/nmap
+
+NMAP_VER=${NMAP_VER_FOLDER%/}
+NMAP_VER=${NMAP_VER#nmap-}
+echo "-- nmap version is $NMAP_VER"
+
+echo "--- Build nmap..."
+cd $DIR/nmap
+./configure --with-nmap --with-ncat --with-nping --without-ndiff --without-zenmap --prefix /usr
+make install -j 4 DESTDIR=$DIR/AppDir
+
+echo "--- Packaging AppImage..."
+cd $DIR
+export VERSION=$NMAP_VER
+./linuxdeploy --appimage-extract-and-run --appdir AppDir -d nmap.desktop -i none.svg --custom-apprun AppRun --output appimage
